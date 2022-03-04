db7bd75a14ff9b51696ced7345b12d79b2034c76
PatrickY567 <Patrick2906@github.com>
2022-03-04 17:37:35 +0800

    update command button

--- Frame/FrameSpecialCmd.py
+++ Frame/FrameSpecialCmd.py
@@ -6,8 +6,11 @@
     -*- coding: utf-8 -*-
 '''
 import subprocess
+import tkinter.ttk
 from tkinter.messagebox import showinfo
 from tkinter import *
+from tkinter.ttk import Combobox
+import sys
 
 INFO_WINDOW_WIDTH = 60
 INFO_WINDOW_HEIGHT = 25
@@ -15,12 +18,19 @@ INFO_WINDOW_HEIGHT = 25
 CMD_WINDOW_WIDTH = INFO_WINDOW_WIDTH
 CMD_WINDOW_HEIGHT = 20
 
+COMMAND_TABLE = {
+    '1': "find",
+    '2': "get",
+    '3': "clean"
+}
+
 
 class Frame_SpecialCmd(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.root = master
         self.creatPage()
+        self.infoIndex = 0
 
     def creatPage(self):
         #
@@ -39,7 +49,7 @@ class Frame_SpecialCmd(Frame):
         self.yscrollbar_info = Scrollbar(self.messages_frame_info, orient=VERTICAL)
         self.yscrollbar_info.grid(row=0, column=8, rowspan=19, sticky=N + S + W)
 
-        # list box to stroe the previous message **row2**
+        # list box to store the previous message **row2**
         self.message_list_info = Listbox(self.messages_frame_info, height=24, width=60, bd=0,
                                          yscrollcommand=self.yscrollbar_info.set,
                                          xscrollcommand=self.xscrollbar_info.set)
@@ -47,27 +57,17 @@ class Frame_SpecialCmd(Frame):
         self.xscrollbar_info.config(command=self.message_list_info.xview)
         self.yscrollbar_info.config(command=self.message_list_info.yview)
 
-        # command label frames of tbox update **row20**
-        # self.cmd_frame_dmclient = LabelFrame(self.root, text='dmclient commands', width=INFO_WINDOW_HEIGHT, labelanchor=N)
-        # self.cmd_frame_dmclient.grid(row=45, column=0, rowspan=5, columnspan=8)
-        #
-        # # input label showed below **row21**
-        # self.cmd_label_dmclient = Label(self.cmd_frame_dmclient, text='start/stop real-time output',
-        #                                 width=CMD_WINDOW_WIDTH, padx=10, pady=5)
-        # self.cmd_label_dmclient.config(fg="blue")
-        # self.cmd_label_dmclient.grid(row=46, column=0, columnspan=8)
-        #
-        # # to send out the message **row22**
-        # self.dmclient_start_button = Button(self.cmd_frame_dmclient, text="start updating result", padx=10, pady=5,
-        #                                     command=self.startGetting_DmclientLog())
-        # self.dmclient_start_button.grid(row=47, column=0, columnspan=8,
-        #                                 sticky=W + E + N + S)
-        #
-        # # clear **row23**
-        # self.dmclient_stop_button = Button(self.cmd_frame_dmclient, text="stop updating", padx=10, pady=5,
-        #                                    command=self.startGetting_DmclientLog)
-        # self.dmclient_stop_button.grid(row=48, column=0, columnspan=8,
-        #                                sticky=W + E + N + S)
+        self.strVar = StringVar()
+        self.command_box = Combobox(self.root, textvariable=self.strVar)
+        command_list = list(COMMAND_TABLE.values())
+        self.command_box["values"] = command_list   # fill the command in combobox
+        self.command_box.current(0)
+        # self.command_box.bind("<<ComboboxSelected>>", self.getBox)
+        self.command_box.grid(row=22, column=0, columnspan=5, sticky=W + E + N + S)
+
+        self.get_button = Button(self.root, text="get Text ", padx=10, pady=5,
+                                 command=self.getBox)
+        self.get_button.grid(row=22, column=5, columnspan=2, sticky=W + E + N + S)
 
     '''
     send message sample
@@ -106,13 +106,16 @@ class Frame_SpecialCmd(Frame):
         new_msg = ''
         print("")
 
-    def upadteMessage(self):
-        new_msg = ''
-        new_msg = self.input_entry.get()
-        self.input_entry.delete(0, END)  # clear inupt message
+    def get_key2int(self, dct, value) -> int:
+        return int([k for (k, v) in dct.items() if v == value][0], 10)
 
-        self.message_list_tbxUpdate.insert(END, new_msg)
-        self.message_list_tbxUpdate.see(END)  # show last line when text overflow
+    def updateMessage(self, message):
+        # new_msg = ''
+        # new_msg = self.input_entry.get()
+        # self.input_entry.delete(0, END)  # clear inupt message
+
+        self.message_list_info.insert(END, message)
+        self.message_list_info.see(END)  # show last line when text overflow
 
     def sendMessage(self):
         newMsg = ''
@@ -127,6 +130,62 @@ class Frame_SpecialCmd(Frame):
 
     def clearMessage(self):
         self.message_list_tbxUpdate.delete(0, END)
+
+    def getBox(self):
+        cmd_str = self.command_box.get()
+        '''# other way to get'''
+        # print(self.strVar.get())
+        cmd_number = self.get_key2int(COMMAND_TABLE, cmd_str)
+        self.executeCommand(cmd_number)
+
+    def executeCommand(self, command):
+        cmd = ShellCommands()
+        status, message = cmd.getCommand(command)
+        self.last_status = status
+        self.last_message = message
+        print("status {}".format(status))
+        if status == 0:
+            self.updateMessage(message)
+
+
+class ShellCommands(object):
+    def commands_1(self):
+        print("executing command1")
+        ret_message = ''
+        cmd = "adb get state"
+
+        try:
+            r0 = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ret_message = r0.communicate()[0]  # [0] output message, [1] error message
+        except OSError as err:
+            print(err, file=sys.stderr)
+            ret_message = err
+        return 0, ret_message
+
+    def commands_2(self):
+        print("executing command2")
+        ret_message = ''
+        cmd = "ipconfig"
+        try:
+            r0 = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ret_message = r0.communicate()[0]  # [0] output message, [1] error message
+        except OSError as err:
+            print(err, file=sys.stderr)
+            ret_message = err
+        return 0, ret_message
+
+    def commands_3(self):
+        ret_message = ''
+        print("executing command3")
+
+
+
+        return 0, ret_message
+
+    def getCommand(self, no):
+        name_of_method = "commands_" + str(no)
+        method = getattr(self, name_of_method, lambda: -1)
+        return method()
 
 
 root = Tk()
