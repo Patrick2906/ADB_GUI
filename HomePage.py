da39267e596454653b14eed81a5114bf3f07559d
PatrickY567 <Patrick2906@github.com>
2022-03-10 15:39:29 +0800

    use app.run fucntion to interact with "HomePage" and "DebugPage"

--- HomePage.py
+++ HomePage.py
@@ -6,7 +6,6 @@
     -*- coding: utf-8 -*-
 '''
 
-
 '''
     -------------
     | ADB debug |       -> to use adb command to enable adb port
@@ -19,7 +18,7 @@ import subprocess
 from tkinter.messagebox import showinfo
 from tkinter import *
 import sys
-from DebugPage import *
+from config import *
 
 MAIN_PAGE_SIZE = (300, 180)
 
@@ -27,6 +26,7 @@ MAIN_PAGE_SIZE = (300, 180)
 class HomePage(object):
     def __init__(self, master=None):
         self.page = None
+        self.status = 0
         self.root = master
         # self.root.geometry('%dx%d' % (300, 180))  # MainPage size
         self.createPage()
@@ -36,13 +36,13 @@ class HomePage(object):
         self.page.grid(padx=100, pady=60, sticky=W + E + N + S)
         # self.page.pack()  # pack the components
 
-        self.connectButton = Button(self.page, text='ADB Connect', width=12, command=self.AdbConnect)
+        self.connectButton = Button(self.page, text='ADB Connect', width=12, command=self.adbConnect)
         self.connectButton.grid(stick=N, pady=10)
 
-        self.quitButton = Button(self.page, text='Quit', width=12, command=self.page.quit)
+        self.quitButton = Button(self.page, text='Quit', width=12, command=self.homePageRun)
         self.quitButton.grid(stick=N, pady=10)
 
-    def AdbConnect(self):
+    def adbConnect(self):
         cmd_valid = False
         adb_cmd = "adb devices"
         adb_cmd = 'ipconfig'
@@ -50,7 +50,7 @@ class HomePage(object):
 
         try:
             r0 = subprocess.Popen(adb_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out = r0.communicate()[0]   # [0] output message, [1] error message
+            out = r0.communicate()[0]  # [0] output message, [1] error message
             cmd_valid = True
         except OSError as err:
             print(err, file=sys.stderr)
@@ -59,23 +59,28 @@ class HomePage(object):
         if cmd_valid:
             if out != '':  # check in the list
                 print("list found")
-                print(out.decode("gbk"))    # todo: utf-8? short term solution with Chinese windows system
-                self.page.pack_forget()
-                DebugPage(self.root)
-
+                print(out.decode("gbk"))  # todo: utf-8? short term solution with Chinese windows system
+                self.connectButton.grid_remove()
+                self.quitButton.grid_remove()
+                self.page.grid_remove()
+                self.page.quit()
+                self.status = PAGE_STATUS_SWITCHPAGE  # exit page number 1
             else:
                 self.emptyList()
 
         print('debug')
 
-
     def emptyList(self):
         print('device not found')
         showinfo('Message', 'device not found')
 
+    def homePageRun(self):
+        self.page.quit()
+        self.status = PAGE_STATUS_INIT
+        return self.status
 
-root = Tk()
-root.title('adb test')
-
-HomePage(root)
-root.mainloop()
+# root = Tk()
+# root.title('adb test')
+#
+# HomePage(root)
+# root.mainloop()
--- run.py
+++ run.py
@@ -5,5 +5,40 @@
     @Software:      PyCharm
     -*- coding: utf-8 -*-
 '''
+from DebugPage import *
+from HomePage import *
+from config import *
 
 
+class APP(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.page = None
+        self.pageID = 0  # initial
+
+    def run(self, initialPage, id):
+        self.page = initialPage(self)
+        self.pageID = id
+        while True:
+            self.mainloop()
+            if self.page.status == PAGE_STATUS_SWITCHPAGE:
+                self.setPage()
+            elif self.page.status == PAGE_STATUS_ERR:
+                print("error condition occurred")
+                break
+            else:
+                break
+
+    def setPage(self):
+        if self.pageID == PAGE_ID_HOME:
+            self.page = DebugPage(self)
+            self.pageID = PAGE_ID_DEBUG     # set new page id
+        elif self.pageID == PAGE_ID_DEBUG:
+            self.page = HomePage(self)
+            self.pageID = PAGE_ID_HOME      # set new page id
+
+
+_app = APP()
+
+if __name__ == '__main__':
+    _app.run(HomePage, PAGE_ID_HOME)
