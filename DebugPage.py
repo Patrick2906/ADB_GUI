ae18a478e53e63c90f9941ea2acb4eb8288c01fa
PatrickY567 <Patrick2906@github.com>
2022-03-10 15:36:10 +0800

    use background scheduler to jump of the debug page

--- DebugPage.py
+++ DebugPage.py
@@ -30,15 +30,16 @@
     clean cache and to delete old certifications
     
 '''
-
 import subprocess
 from tkinter.messagebox import showinfo
 from tkinter import *
-from apscheduler.schedulers.background import BlockingScheduler
 from datetime import datetime
 from Frame.FrameOtaLog import *
 from Frame.FrameSpecialCmd import *
 import functools
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from config import *
 
 DEBUG_PAGES = 5  # config the total pages
 OTALOG_PAGE_ID = 0
@@ -48,20 +49,37 @@ SPECIALCOMMAND_PAGE_ID = 1
 class DebugPage(object):
     def __init__(self, master=None):
         self.root = master
+        self.status = 0
+        self.p0 = None  # process for connection check
+        print("status: {}".format(self.status))
         # self.root.geometry('%dx%d' % (800, 600))
         self.createPage()
-
+        self.scheduler = BackgroundScheduler()
+        self.jobTestPresent = self.scheduler.add_job(self.tick, trigger='interval', seconds=1)
+        self.scheduler.start()
+        print("scheduler start")
+        self.cnt = 0
 
     def createPage(self):
         self.otaLogPage = Frame_OtaLog(self.root)
         self.specialCmdPage = Frame_SpecialCmd(self.root)
 
         self.specCmdPageActive()  # default page
-        menubar = Menu(self.root)
-        menubar.add_command(label="special command", command=self.specCmdPageActive)
-        menubar.add_command(label="ota log", command=self.otaLogPageActive)
+        self.menubar = Menu(self.root)
+        self.menubar.add_command(label="special_command", command=self.specCmdPageActive)
+        self.menubar.add_command(label="ota_log", command=self.otaLogPageActive)
 
-        self.root["menu"] = menubar  # set menu bar
+        self.root["menu"] = self.menubar  # set menu bar
+
+    # to test the background scheduler
+    def tick(self):
+        print('Tick! The time is: %s' % datetime.now())
+        self.cnt += 1
+        if self.cnt > 5:
+            self.scheduler.pause_job(self.jobTestPresent.id)
+            print("going to disconnect")
+            self.adbDisconnected()
+            self.scheduler.resume()
 
     def pageActive(func):
         @functools.wraps(func)
@@ -79,13 +97,26 @@ class DebugPage(object):
     def specCmdPageActive(self):
         self.specialCmdPage.createPage()
 
-    def pagePack(self, pageId):
-        self.last_page_id = pageId
+    @pageActive
+    def adbDisconnected(self):
+        self.cnt = 0
+        self.scheduler.remove_all_jobs()
+        self.menubar.destroy()
+        # self.scheduler.shutdown()
+        print("scheduler stop")
+        abc = showinfo('Message', 'device disconnected ')
+        self.status = PAGE_STATUS_SWITCHPAGE
+        self.root.quit()
+        print("quit root")
+
+
+    def pagePack(self, frameId):
+        self.last_frame_id = frameId
 
 
 
 # root = Tk()
-# root.title('debug page')
+# root.title('adb test')
 #
-# DebugPage(root)
+# HomePage(root)
 # root.mainloop()
---
+++ config.py
@@ -0,0 +1,15 @@
+'''
+    @Create time:   2022/3/10 14:29
+    @Autohr:        Patrick.Yang
+    @File:          config.py.py
+    @Software:      PyCharm
+    -*- coding: utf-8 -*-
+'''
+
+# Symbolic for Page config
+PAGE_STATUS_ERR = -1  # error status
+PAGE_STATUS_INIT = 0  # initial state(normal state)
+PAGE_STATUS_SWITCHPAGE = 1  # indicate to switch to other page
+
+PAGE_ID_HOME = 1  # home page when start
+PAGE_ID_DEBUG = 2  # debug page after connected
